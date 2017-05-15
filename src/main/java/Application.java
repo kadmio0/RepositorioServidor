25f01ccdec9b2ec8d1f05268103562325d3f7f79
jccr2 <jorgechavezrnd@gmail.com>
2017-05-15 11:35:57 -0400

    first commit, Jorge

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,6 +3,7 @@
  */
 public class Application {
     public static void main(String[] args) {
-        System.out.printf("Hola mundo, Kevin");
+        System.out.printf("Hola mundo, Kevin\n");
+        System.out.println("Hello World, Jorge");
     }
 }
