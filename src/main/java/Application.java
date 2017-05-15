19660e9938d97aadc16af7908820b758c27e975d
Samuel_UCB <val97samuel@gmail.com>
2017-05-15 13:01:28 -0400

    First commit, Samuel

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,5 +5,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.printf("Hola mundo, Kevin\n");
         System.out.println("Hello World, Jorge");
+        System.out.println("Hello World, Samuel");
     }
 }
