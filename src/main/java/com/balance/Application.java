fb6f4eae15e49a614dcf0cb017e21cf3490b73a7
Jorge Chavez <jorgechavezrnd@gmail.com>
2017-05-20 00:07:30 -0400

    Second commit, Jorge: Application | Test commit from intellij idae

--- src/main/java/com/balance/Application.java
+++ src/main/java/com/balance/Application.java
@@ -3,16 +3,12 @@ package com.balance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.Console;
+import java.io.Console; // Esto no se utiliza
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hola soy Toki");
-        System.out.println("Hola soy Daniel");
-        System.out.println("Hola soy Jorge");
-        System.out.println("Hola soy Kevin");
         SpringApplication.run(Application.class, args);
     }
 }
