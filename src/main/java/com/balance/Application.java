21d3dba0cb752867de527e7c504a86ed4c8fbb0c
Jorge Chavez <jorgechavezrnd@gmail.com>
2017-05-23 19:38:07 -0400

    Fourth commit, Jorge: user/home.html, profile.html | Complete logout for all users

--- src/main/java/com/balance/Application.java
+++ src/main/java/com/balance/Application.java
@@ -3,7 +3,7 @@ package com.balance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.Console; // Esto no se utiliza
+import java.io.Console;
 
 @SpringBootApplication
 public class Application {
